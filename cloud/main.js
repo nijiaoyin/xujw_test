5f8abdd2062c1735f414e633f8edfa342195ca61
jiwei.xjw <jiwei.xjw@alibaba-inc.com>
2015-04-21 09:38:30 +0800

    new function

--- cloud/main.js
+++ cloud/main.js
@@ -3,3 +3,16 @@
 AV.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+AV.Cloud.define('content_find', function(request, response) {
+  var query = new AV.Query('Post');
+  query.equalTo('content', request.params.content);
+  query.find({
+    success: function(results) {
+      response.success(results.length);
+    },
+    error: function() {
+      response.error('no such item');
+    }
+  });
+});
